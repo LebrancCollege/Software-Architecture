badd8dc4647b7e9ffc144c92e1b0b15441bc9a4f
lebrancconvas <lebrancconvas@gmail.com>
2022-12-07 01:16:04 +0700

    🎉 feat(init): init client idea 02

---
+++ idea02/client/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return(
+    <div>
+      <Head>
+        <title>CS Quiz</title>
+      </Head>
+      <div>
+        <header>
+          <div>
+            <h1>CS Quiz</h1> 
+          </div>
+        </header>
+      </div>
+    </div>
+  )
+}
